5666833eb86f82d858a81e343b7b800d4fd74c6c
KristinaFedorova <kristinka.fedorova.04@gmail.com>
2022-09-18 16:44:51 +0200

    first_add

---
+++ 10_years/10_years.js
@@ -0,0 +1,63 @@
+const starYear=document.getElementById("start_year")
+const price=document.getElementById("price")
+const saving=document.getElementById("saving")
+const increase=document.getElementById("increase")
+const degression=document.getElementById("degression")
+
+const btnSend=document.getElementById("input")
+
+btnSend.onclick=function(){
+let expPercent=document.getElementById('percent')
+expPercent=price
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
