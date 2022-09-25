31efadaea0d109bf48e1251ec33eca85bd035f90
KristinaFedorova <kristinka.fedorova.04@gmail.com>
2022-09-25 16:48:40 +0200

    add_assetsSat+acquiredSat

--- 10_years/10_years.js
+++ 10_years/10_years.js
@@ -11,6 +11,9 @@ btnSend.onclick = function () {
     pricePercent()
     bitPrice()
     savings()
+    acquired()
+    assetsSat()
+    acquiredSat()
 }
 
 function years() {
@@ -52,48 +55,50 @@ function savings() {
 }
 
 function bitPrice() {
-
     let pricePercent = document.querySelectorAll('.price_percent')
-    // let b = Number(increase.value) / 100
-    // pricePercent[0].innerHTML = '#'
-    // pricePercent[1].innerHTML = b * 100
-    // let c = 1 - (Number(degression.value) / 100)
-    // console.log(c)
-
     let bitPrice = document.querySelectorAll('.bit_price')
     let a = Number(price.value)
-    console.log(a)
     bitPrice[0].innerHTML = a
-    console.log(a)
 
     for (let i = 1; i < 10; i++) {
-        // console.log(pricePercent[i].value)
-        // let d = Number(pricePercent[i]/100)
-        // console.log(b)
-        // bitPrice[i].innerHTML = a * (1 + d)
-        // a = a * (1 + d)
-        // console.log(a)
-
-        // if (b * c * 100 > 3) {
-        //     pricePercent[i+1 ].innerHTML = Math.floor(b * c * 100)
-        //     b = b * c
-            let d = pricePercent[i].innerHTML / 100
-            bitPrice[i].innerHTML = Math.floor(a * (1 + d))
-            a = a * (1 + d)
-
-
-
-            // } else {
-            //     pricePercent[i+1].innerHTML = 3
-            //     let d = Number(pricePercent[i+1]/100)
-            //     bitPrice[i].innerHTML = a * (1 + d)
-            //     a = a * (1 + d)
-            // }
-        }
-
+        let d = pricePercent[i].innerHTML / 100
+        bitPrice[i].innerHTML = Math.floor(a * (1 + d))
+        a = a * (1 + d)
+    }
 }
 
-function acquired(){
+function acquired() {
+    let bitPrice = document.querySelectorAll('.bit_price')
+    let savings = document.querySelectorAll('.saving')
+    let acquired = document.querySelectorAll('.acquired')
+    for (let i = 0; i < 10; i++) {
+        acquired[i].innerHTML = Math.floor(savings[i].innerHTML / bitPrice[i].innerHTML * 100000000)
+    }
+}
+
+function assetsSat() {
+    let assetsSat = document.querySelectorAll('.assets')
+    let acquired = document.querySelectorAll('.acquired')
+    assetsSat[0].innerHTML = acquired[0].innerHTML
+    assetsSat[0].innerHTML = Number(assetsSat[0].innerHTML)
+    let b = Number(assetsSat[0].innerHTML)
+    for (let i = 1; i < 10; i++) {
+        assetsSat[i].innerHTML = b + Number(acquired[i].innerHTML)
+        b = Number(assetsSat[i].innerHTML)
+    }
+}
+
+function acquiredSat() {
+    let acquiredSat = document.querySelectorAll('.acquired_sat')
+    let acquired = document.querySelectorAll('.acquired')
+    let assetsSat = document.querySelectorAll('.assets')
+    let a = Number(assetsSat[9].innerHTML)
+    console.log(a)
+    for (let i = 0; i < 10; i++) {
+        let b = Number(acquired[i].innerHTML)
+        let result = 100 * b / a
+        acquiredSat[i].innerHTML = result.toFixed(2)
+    }
 
 }
 
